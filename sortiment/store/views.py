1b899c8a9fca4e1a911f98de3db089ae809de409
Sabinka <ssamporova@gmail.com>
2023-01-30 15:33:49 +0100

    add products view

--- sortiment/store/views.py
+++ sortiment/store/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product, Warehouse, WarehouseState
+
+def product_list(request): # , warehouse_name):
+
+    # TODO send state of product in selected state
+
+
+    prods = Product.objects.all()
+    context = {
+        "prods": prods
+    }
+    return render(request, "store/products.html", context)
+
 
 # Create your views here.
