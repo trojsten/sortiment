2398e3aaa0ea4a37a81966ae8c79826f1c19b71a
Jakub Šiagi <jakub.siagi@gmail.com>
2023-01-30 19:59:41 +0100

    Tag filter

--- sortiment/store/views.py
+++ sortiment/store/views.py
@@ -3,11 +3,20 @@ from collections import defaultdict
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Warehouse, WarehouseState, WarehouseEvent
+from .models import Product, Warehouse, WarehouseState, WarehouseEvent, Tag
 
 
 def product_list(request):
     warehouse_id = request.GET.get("warehouse_id", 1)
+
+    tags = []
+    for tag in Tag.objects.all():
+        tags.append({
+            "name": tag.name,
+            "active": "tag-%s" % tag.name in request.GET
+        })
+    active_tags = [tag["name"] for tag in tags if tag["active"]]
+
 
     w_states = WarehouseState.objects.filter(warehouse__exact=warehouse_id)
     state_d = defaultdict(lambda: 0)
@@ -20,13 +29,20 @@ def product_list(request):
     for st in all_states:
         all_state_d[st["product"]] = st["totqty"]
 
+    # only show products that have all active tags
+
     prods = Product.objects.all()
+    for tag in active_tags:
+        prods = prods.filter(tags__name__contains=tag)
     for p in prods:
         p.qty = state_d[p.id]
         p.totqty = all_state_d[p.id]
 
+
+
     context = {
-        "prods": prods
+        "prods": prods,
+        "tags": tags,
     }
     return render(request, "store/products.html", context)
 
