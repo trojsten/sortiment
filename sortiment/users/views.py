8841c3315f19f31868df733d16b374f2183a6313
Sabinka <ssamporova@gmail.com>
2023-01-31 21:13:23 +0100

    refactoring, creating django views instead of funs

--- sortiment/users/views.py
+++ sortiment/users/views.py
@@ -1,77 +1,70 @@
 from django.contrib.auth import login, logout
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, FormView, ListView
 
 from .admin import UserCreationForm
 from .forms import CreditAddAndWithdrawalForm, CreditMovementForm
 from .models import SortimentUser
 
 
-def user_list(request):
-    context = {"users": SortimentUser.objects.all().order_by("-is_guest", "username")}
-    return render(request, "users/users_list.html", context)
+class UserListView(ListView):
+    template_name = "users/users_list.html"
+    queryset = SortimentUser.objects.order_by("-is_guest", "username")
+    context_object_name = "users"
 
 
-def login_user(request, user_id):
-    user = SortimentUser.objects.get(id=user_id)
-    login(request, user)
-    return HttpResponseRedirect(reverse("store:product_list"))
+class LoginUserView(View):
+    def get(self, request, user_id):
+        user = SortimentUser.objects.get(id=user_id)
+        login(request, user)
+        return HttpResponseRedirect(reverse("store:product_list"))
 
 
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("user_list"))
+class LogoutUserView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse("user_list"))
 
 
-def create_user(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse("user_list"))
-    else:
-        form = UserCreationForm()
-    context = {"form": form}
-    return render(request, "users/create.html", context)
+class CreateUserView(CreateView):
+    template_name = "users/create.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("user_list")
 
 
-def credit(request):
-    if request.method == "POST":
-        credit_movement_form = CreditMovementForm(request.POST)
-        if credit_movement_form.is_valid():
-            user = request.user
-            if not isinstance(user, SortimentUser) or user.is_guest:
-                return
-            money = -credit_movement_form.cleaned_data.get("credit")
-            if user.can_pay(money):
-                user2 = credit_movement_form.cleaned_data.get("user")
-                user.make_credit_operation(-money)
-                user2.make_credit_operation(money)
-                return HttpResponseRedirect(reverse("store:product_list"))
-            else:
-                credit_movement_form.add_error("credit", "Nemáš na to dosť peňazí")
+class CreditMovementView(FormView):
 
-        credit_add_and_withdrawal_form = CreditAddAndWithdrawalForm(request.POST)
-        if credit_add_and_withdrawal_form.is_valid():
-            user = request.user
-            if not isinstance(user, SortimentUser) or user.is_guest:
-                return
-            money = credit_add_and_withdrawal_form.cleaned_data.get("credit")
-            if user.can_pay(money):
-                user.make_credit_operation(money)
-                return HttpResponseRedirect(reverse("store:product_list"))
-            else:
-                credit_add_and_withdrawal_form.add_error(
-                    "credit", "Nemáš na to dosť peňazí"
-                )
-    else:
-        credit_add_and_withdrawal_form = CreditAddAndWithdrawalForm()
+    form_class = CreditMovementForm
+    template_name = "users/creditmovement.html"
 
-        credit_movement_form = CreditMovementForm()
+    def form_valid(self, form):
+        user = self.request.user
+        if user.is_guest:
+            return
+        money = -form.cleaned_data.get("credit")
+        if user.can_pay(money):
+            user2 = form.cleaned_data.get("user")
+            user.make_credit_operation(-money)
+            user2.make_credit_operation(money)
+            return HttpResponseRedirect(reverse("store:product_list"))
+        else:
+            form.add_error("credit", "Nemáš na to dosť peňazí")
 
-    context = {
-        "credit_movement_form": credit_movement_form,
-        "credit_add_and_withdrawal_form": credit_add_and_withdrawal_form,
-    }
-    return render(request, "users/credit_movement.html", context)
+
+class CreditAddWithdrawalView(FormView):
+
+    form_class = CreditAddAndWithdrawalForm
+    template_name = "users/credaddwithdrowal.html"
+
+    def form_valid(self, form):
+        user = self.request.user
+        if not isinstance(user, SortimentUser) or user.is_guest:
+            return
+        money = form.cleaned_data.get("credit")
+        if user.can_pay(money):
+            user.make_credit_operation(money)
+            return HttpResponseRedirect(reverse("store:product_list"))
+        else:
+            form.add_error("credit", "Nemáš na to dosť peňazí")
