a8ca6a737f94f59e3376f504d3cb4449d9f23174
Marcel Palaj <palaj.marcel@gmail.com>
2023-02-01 16:33:13 +0100

    update store admin display

--- sortiment/store/admin.py
+++ sortiment/store/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from store import models
 
-admin.site.register(models.Product)
-admin.site.register(models.Warehouse)
-admin.site.register(models.WarehouseState)
-admin.site.register(models.WarehouseEvent)
+class ProductAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "barcode",
+        "price",
+        "is_unlimited",
+        "is_dummy",
+    )
+    list_filter = ("is_unlimited","is_dummy",)
+
+class WarehouseAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "ip",
+    )
+
+class WarehouseState(ModelAdmin):
+    list_display = (
+        "warehouse",
+        "product",
+        "quantity",
+        "total_price",
+    )
+
+class WarehouseEventAdmin(ModelAdmin):
+    list_display = (
+        "product",
+        "warehouse",
+        "quantity",
+        "price",
+        "timestamp",
+        "type",
+        "user",
+    )
+    list_filter = ("type",)
+
+
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Warehouse, WarehouseAdmin)
+admin.site.register(models.WarehouseState, WarehouseState)
+admin.site.register(models.WarehouseEvent, WarehouseEventAdmin)
 admin.site.register(models.Tag)
