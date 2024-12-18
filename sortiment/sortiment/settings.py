576bacade229dc4e409b52557de109ad87c60ad4
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2024-12-18 17:25:57 +0100

    fixes

--- sortiment/sortiment/settings.py
+++ sortiment/sortiment/settings.py
@@ -123,8 +123,6 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = "users.SortimentUser"
 LOGIN_URL = "/"
 
-DEBUG_TOOLBAR_CONFIG = {"ROOT_TAG_EXTRA_ATTRS": "data-turbo-permanent"}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
